e59f1a9fd0410d66c686782ee29a265ad2dd6613
Victor Castor <55765661+victorcastorgui@users.noreply.github.com>
2021-11-27 00:39:36 -0500

    Add files via upload

---
+++ victorcastorgui-cs1a-project01/README.txt
@@ -0,0 +1,15 @@
+project folder:
+victorcastorgui-cs1a-project01/
+
+
+Brief description of submitted files:
+
+src/DemoGit.java
+    - Includes main() for debugging.
+
+
+resources/RUN.txt
+    - console output of name.
+
+README.txt
+    - description of submitted files
---
+++ victorcastorgui-cs1a-project01/src/DemoGit.java
@@ -0,0 +1,23 @@
+/**
+ * Simple class to test submitting to GitHub repository.
+ * @author Foothill College, Bita M, Victor Castor.
+ *
+ * Note: If you modified this file then update the author name with your: FirstName LastName
+ * 
+ * Reminder: Take a look at "Example Program Submission" in course Modules and follow the
+ *           "Program Guidelines" to make sure your submission is complete.
+ */
+public class DemoGit 
+{
+
+	/**
+	 * Outputs my name.
+	 * @param args Not used.
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello. My name is Victor Castor.");
+
+	}
+
+}
